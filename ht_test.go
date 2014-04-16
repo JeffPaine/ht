ee2d6b195c577bca9450d35510f348b9c0af190a
Jeffrey Paine <jeffreyalanpaine@gmail.com>
2014-04-15 21:51:02 -0400

    Add basic test for /ip

--- ht_test.go
+++ ht_test.go
@@ -16,3 +16,14 @@ func TestIndexReturns200StatusCode(t *testing.T) {
 		t.Fatalf("Received unexpected status code of %v", response.Code)
 	}
 }
+
+func TestIp(t *testing.T) {
+	request, _ := http.NewRequest("GET", "/ip", nil)
+	response := httptest.NewRecorder()
+
+	ip(response, request)
+
+	if response.Code != http.StatusOK {
+		t.Fatalf("Received unexpected status code of %v", response.Code)
+	}
+}
