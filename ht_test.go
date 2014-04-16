094b59568e081cb69c2121c1ac3757d6514e9a0d
Jeffrey Paine <jeffreyalanpaine@gmail.com>
2014-04-15 21:44:37 -0400

    Add first test Related to #4

---
+++ ht_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"testing"
+	"net/http"
+	"net/http/httptest"
+	)
+
+func TestIndexReturns200StatusCode(t *testing.T) {
+	request, _ := http.NewRequest("GET", "/", nil)
+	response := httptest.NewRecorder()
+
+	index(response, request)
+
+	if response.Code != http.StatusOK {
+		t.Fatalf("Received unexpected status code of %v", response.Code)
+	}
+}
