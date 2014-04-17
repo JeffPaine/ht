ccb72cd4750c693a2f7a736213e122c482733db4
Jeffrey Paine <jeffreyalanpaine@gmail.com>
2014-04-17 16:06:25 -0400

    Add basic tests for all handlers

--- ht_test.go
+++ ht_test.go
@@ -17,11 +17,44 @@ func TestIndexReturns200StatusCode(t *testing.T) {
 	}
 }
 
-func TestIp(t *testing.T) {
+func TestIpReturns200StatusCode(t *testing.T) {
 	request, _ := http.NewRequest("GET", "/ip", nil)
 	response := httptest.NewRecorder()
 
 	ip(response, request)
+
+	if response.Code != http.StatusOK {
+		t.Fatalf("Received unexpected status code of %v", response.Code)
+	}
+}
+
+func TestUserAgentReturns200StatusCode(t *testing.T) {
+	request, _ := http.NewRequest("GET", "/user-agent", nil)
+	response := httptest.NewRecorder()
+
+	userAgent(response, request)
+
+	if response.Code != http.StatusOK {
+		t.Fatalf("Received unexpected status code of %v", response.Code)
+	}
+}
+
+func TestHeadersReturns200StatusCode(t *testing.T) {
+	request, _ := http.NewRequest("GET", "/headers", nil)
+	response := httptest.NewRecorder()
+
+	headers(response, request)
+
+	if response.Code != http.StatusOK {
+		t.Fatalf("Received unexpected status code of %v", response.Code)
+	}
+}
+
+func TestGetReturns200StatusCode(t *testing.T) {
+	request, _ := http.NewRequest("GET", "/get", nil)
+	response := httptest.NewRecorder()
+
+	get(response, request)
 
 	if response.Code != http.StatusOK {
 		t.Fatalf("Received unexpected status code of %v", response.Code)
